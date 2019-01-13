9f27bd37fc8584672b4ba8829b7369a8beb59eea
griffin <sample@homawebnama.com>
2019-01-13 12:35:14 +0330

    container format is rectified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import InputSelectContainer from './compoents/InputSelect/InputSelectContainer';
+import './App.css';
 
 class App extends Component {
 	render() {
